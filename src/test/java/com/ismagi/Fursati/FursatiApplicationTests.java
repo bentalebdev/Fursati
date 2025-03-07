9c875b5d4d2853972930ac764cb16d320dd2f125
bentalebdev <bentalebmoulaymhamed@gmail.com>
2025-03-07 21:40:30 +0000

    Initial commit

---
+++ src/test/java/com/ismagi/Fursati/FursatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismagi.Fursati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FursatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
